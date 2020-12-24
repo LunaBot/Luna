49d581d02caf7946f8ee884627327f4221578539
Alexis Tyler <xo@wvvw.me>
2020-12-24 22:21:39 +1030

    fix: add database settings

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,8 @@
 import createConnectionPool from '@databases/pg';
 import { config } from './config';
 
-export const database = createConnectionPool(config.CONNECTION_STRING);
+export const database = createConnectionPool({
+    connectionString: config.CONNECTION_STRING,
+    idleTimeoutMilliseconds: 30000,
+    maxUses: 20,
+});
