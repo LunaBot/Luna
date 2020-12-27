be29127de991f1d1ec4aad214adc6369a855374a
Alexis Tyler <xo@wvvw.me>
2020-12-28 07:49:43 +1030

    fix: return correct prefix for "commands"

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -2,6 +2,9 @@ import { MessageEmbed, Message } from 'discord.js';
 import commands from './index';
 import { getCommandHelp } from '../utils';
 import { Command } from '../command';
+import { Server } from '../servers';
+import { isTextChannelMessage } from '../guards';
+import { AppError } from '../errors';
 
 class Commands extends Command {
     public name = 'commands';
@@ -14,17 +17,22 @@ class Commands extends Command {
     public roles = [ '@everyone' ];
 
     async handler(_prefix: string, message: Message, _args: string[]) {
-        const _commandsUserCanAccess = (commands as Command[]).filter(command => message.member?.roles.cache.some(role => command.roles.includes(role.name)));
-        const commandsUserCanAccess = _commandsUserCanAccess.filter(command => !command.hidden);
-        const fields = commandsUserCanAccess.map(command => getCommandHelp(command));
+        if (isTextChannelMessage(message)) {
+            const server = await Server.findOrCreate({ id: message.guild.id });
+            const _commandsUserCanAccess = (commands as Command[]).filter(command => message.member?.roles.cache.some(role => command.roles.includes(role.name)));
+            const commandsUserCanAccess = _commandsUserCanAccess.filter(command => !command.hidden);
+            const fields = commandsUserCanAccess.map(command => getCommandHelp(server.prefix || '!', command));
+    
+            const embed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setURL('https://discord.js.org/')
+                .setAuthor('Commands')
+                .addFields(fields);
+    
+            return embed;
+        }
 
-        const embed = new MessageEmbed()
-            .setColor('#0099ff')
-            .setURL('https://discord.js.org/')
-            .setAuthor('Commands')
-            .addFields(fields);
-
-        return embed;
+        throw new AppError('Invalid channel type');
     }
 };
 
