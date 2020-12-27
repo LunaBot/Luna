5fb212fb9a1b2b58360a9ac987ceeb18ad725a8f
Alexis Tyler <xo@wvvw.me>
2020-12-27 21:10:04 +1030

    chore: start on web dashboard

--- src/endpoints/frontend.ts
+++ src/endpoints/frontend.ts
@@ -1,10 +1,33 @@
 import router from 'express-promise-router';
+import { AppError } from '../errors';
+import { Server } from '../servers';
 
 const frontend = router();
 
 // Home page
 frontend.get('/', (_req, res) => {
     res.send('Coming soon...');
+});
+
+// User profile
+frontend.get('/profile/:userId', (_req, res) => {
+    res.send('Coming soon...');
+});
+
+// Server selection
+frontend.get('/dashboard', (_req, res) => {
+    res.send('Coming soon...');
+});
+
+// Dashboards
+frontend.get('/dashboard/:serverId', async (req, res) => {
+    const serverId = req.params.serverId;
+    const server = await Server.Find({ id: serverId });
+    if (!server) {
+        throw new AppError('Invalid server ID');
+    }
+
+    res.send(`Welcome to ${serverId}`);
 });
 
 // Health
