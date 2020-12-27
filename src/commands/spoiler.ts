1b8b02bfd550efcfbc2558b8f3a1717a29fb40b1
Alexis Tyler <xo@wvvw.me>
2020-12-28 08:35:49 +1030

    fix: use displayName for member or fallback to username

--- src/commands/spoiler.ts
+++ src/commands/spoiler.ts
@@ -1,6 +1,9 @@
 import { Message, MessageAttachment, MessageEmbed } from 'discord.js';
 import { Command } from '../command';
 import axios from 'axios';
+import { client } from '../client';
+import { isTextChannelMessage } from '../guards';
+import { AppError } from '../errors';
 
 const match = (string: string) => {
   const groups = string.match(/\[(?<warning>.*)\] (?<message>.*)/i)?.groups;
@@ -32,70 +35,75 @@ class Spoiler extends Command {
   // !spoiler [TW: This is a test post] Well this is neat!
   // !spoiler Well this is neat!
   async handler(_prefix: string, message: Message, _args: string[]) {
-    // Parse message
-    const text = _args.join(' ').trim();
-    const spoilerInfo = match(text);
+    if (isTextChannelMessage(message)) {
+      // Parse message
+      const text = _args.join(' ').trim();
+      const spoilerInfo = match(text);
 
-    // Set main content
-    const content = (spoilerInfo?.message || text).trim();
+      // Set main content
+      const content = (spoilerInfo?.message || text).trim();
 
-    // No chars
-    if (content.length === 0 && message.attachments.size === 0) {
-      return 'You need to provide a description or image.';
-    }
+      // No chars
+      if (content.length === 0 && message.attachments.size === 0) {
+        return 'You need to provide a description or image.';
+      }
 
-    // Only allow 2000 chars
-    if (content.length >= 2000) {
-      return `Couldn't spoiler, too many characters. ${content.length}/2000`;
-    }
+      // Only allow 2000 chars
+      if (content.length >= 2000) {
+        return `Couldn't spoiler, too many characters. ${content.length}/2000`;
+      }
 
-    // Creat message
-    const embed = new MessageEmbed({
-      author: {
-        name: message.author.username,
-        iconURL: message.author.avatarURL({
-          size: 32
-        })?.toString()
-      },
-      description: `||${content}||\n\nPosted by <@!${message.author.id}>`
-    });
-
-    // Create images
-    const images = await Promise.all(message.attachments.mapValues(async attachment => {
-      const image = await getImage(attachment.url);
-      // const extension = attachment.name?.split('.')[attachment.name?.split('.').length - 1];
-      // const fileName = `spoiler_image.${extension}`;
-      // console.log({fileName, extension});
-      return new MessageAttachment(image, `SPOILER_${attachment.name}`, {
-        id: Math.random(),
-        spoiler: true
+      // Create message
+      const member = client.guilds.cache.get(message.guild?.id)?.member(message.author.id);
+      const embed = new MessageEmbed({
+        author: {
+          name: member?.displayName ?? message.author.username,
+          iconURL: message.author.avatarURL({
+            size: 32
+          })?.toString()
+        },
+        description: `||${content}||\n\nPosted by <@!${message.author.id}>`
       });
-      // return {
-      //   ...attachment,
-      //   attachment: attachment.attachment,
-      //   name: `SPOILER_${attachment.name}`,
-      //   spoiler: true
-      // };
-    }).array());
 
-    // Remove original message
-    await message.delete();
+      // Create images
+      const images = await Promise.all(message.attachments.mapValues(async attachment => {
+        const image = await getImage(attachment.url);
+        // const extension = attachment.name?.split('.')[attachment.name?.split('.').length - 1];
+        // const fileName = `spoiler_image.${extension}`;
+        // console.log({fileName, extension});
+        return new MessageAttachment(image, `SPOILER_${attachment.name}`, {
+          id: Math.random(),
+          spoiler: true
+        });
+        // return {
+        //   ...attachment,
+        //   attachment: attachment.attachment,
+        //   name: `SPOILER_${attachment.name}`,
+        //   spoiler: true
+        // };
+      }).array());
 
-    // No content just images
-    if (content.length === 0) {
-      return [...images];
-    }
+      // Remove original message
+      await message.delete();
 
-    // Generic spoiler
-    if (!spoilerInfo) {
-      embed.setTitle(`Spoiler`);
+      // No content just images
+      if (content.length === 0) {
+        return [...images];
+      }
+
+      // Generic spoiler
+      if (!spoilerInfo) {
+        embed.setTitle(`Spoiler`);
+        return [embed, ...images];
+      }
+
+      // Warning
+      embed.setColor('#FF0000');
+      embed.setTitle(spoilerInfo.warning);
       return [embed, ...images];
     }
 
-    // Warning
-    embed.setColor('#FF0000');
-    embed.setTitle(spoilerInfo.warning);
-    return [embed, ...images];
+    throw new AppError('Invalid channel type!');
   }
 };
 
