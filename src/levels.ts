dc07ebc38ce9a08d6b346643f023da6ae90ff8b3
Alexis Tyler <xo@wvvw.me>
2020-12-28 14:16:12 +1030

    chore: lint

--- src/levels.ts
+++ src/levels.ts
@@ -1,17 +1,17 @@
 export class LevelManager {
-    static Equate(experience: number) {
-      return Math.floor(experience + 300 * Math.pow(2, experience / 7));
-    }
-  
-    static LevelToExperience(level: number) {
-      let experience = 0;
-      for (let i = 1; i < level; i++) experience += this.Equate(i);
-      return Math.floor(experience / 4);
-    }
-  
-    static ExperienceToLevel(experience: number) {
-      let level = 0;
-      while (LevelManager.LevelToExperience(level) < experience) level++;
-      return level;
-    }
+  static Equate(experience: number) {
+    return Math.floor(experience + 300 * Math.pow(2, experience / 7));
+  }
+
+  static LevelToExperience(level: number) {
+    let experience = 0;
+    for (let i = 1; i < level; i++) experience += this.Equate(i);
+    return Math.floor(experience / 4);
+  }
+
+  static ExperienceToLevel(experience: number) {
+    let level = 0;
+    while (LevelManager.LevelToExperience(level) < experience) level++;
+    return level;
+  }
 };
