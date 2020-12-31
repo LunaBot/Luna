926cd9013448b0435212c38fdad8615318c7696c
Alexis Tyler <xo@wvvw.me>
2020-12-31 16:53:33 +1030

    feat: more descriptive help

--- src/commands/help/index.ts
+++ src/commands/help/index.ts
@@ -1,11 +1,12 @@
-import { Message } from 'discord.js';
+import dedent from 'dedent';
+import { Message, MessageEmbed } from 'discord.js';
 import { Command } from '../../command';
 import { InvalidCommandError } from '../../errors';
+import commands from '../index';
 import { menu as generateHelp } from './generators';
-import { member as generateMemberRoleHelp, moderator as generateModeratorRoleHelp } from './generators/roles';
 
 class Help extends Command {
-    public name = 'help';
+    public name = 'Help';
     public command = 'help';
     public timeout = 5000;
     public description = 'Show help';
@@ -14,21 +15,48 @@ class Help extends Command {
     public examples = [];
     public roles = [ '@everyone' ];
 
+    private getCommandSummary(prefix: string, command: Command) {
+        return [{
+            name: 'Name',
+            value: command.name,
+        }, {
+            name: 'Command',
+            value: `\`${prefix}${command.command}\``
+        }, {
+            name: 'Description',
+            value: command.description
+        }];
+    }
+
+    private getCommandExamples(prefix: string, command: Command) {
+        return {
+            name: 'Examples',
+            value: '```\n' + command.examples.map(example => `${prefix}${example.substring(1)}`).join('\n') + '\n```'
+        };
+    }
+
     async handler(prefix: string, _message: Message, args: string[]) {
-        const role = args[0];
-        if (!role) {
+        const commandName = args[0]; // verification
+        if (!commandName) {
             return generateHelp(prefix);
         }
 
-        if (role === 'moderator') {
-            return generateModeratorRoleHelp(prefix);
+        const command = (commands as Command[]).find(_command => _command.command === commandName);
+        if (!command) {
+            throw new InvalidCommandError(prefix, commandName, args);
         }
 
-        if (role === 'member') {
-            return generateMemberRoleHelp(prefix);
+        // Create the embed
+        const embed = new MessageEmbed()
+            .setColor('#0099ff')
+            .addFields(this.getCommandSummary(prefix, command))
+
+        // If we have examples show them
+        if (command.examples.length >= 1) {
+            embed.addFields(this.getCommandExamples(prefix, command));
         }
 
-        throw new InvalidCommandError(prefix, 'help', args);
+        return embed;
     }
 };
 
