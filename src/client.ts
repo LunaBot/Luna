6d562f4569cd9ec90f20c56ea7a6a3c73ca703bb
Alexis Tyler <xo@wvvw.me>
2020-12-31 16:54:16 +1030

    fix: patch pending until discord.js adds it
    https://github.com/discordjs/discord.js/issues/5163

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,22 @@
-import { Client } from 'discord.js';
+import { Client, Structures } from 'discord.js';
+
+Structures.extend('GuildMember', GuildMember => {
+  class CoolGuild extends GuildMember {
+    pending = false;
+
+    constructor(client: any, data: any, guild: any) {
+      super(client, data, guild);
+      this.pending = data.pending ?? false;
+    }
+
+    _patch(data: any) {
+        // @ts-expect-error
+        super._patch(data);
+        this.pending = data.pending ?? false;
+    }
+  }
+
+  return CoolGuild;
+});
 
 export const client = new Client();
