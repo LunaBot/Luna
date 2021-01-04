ec96b780b5e3ab88fa6523bf4049c570bebf9445
Alexis Tyler <xo@wvvw.me>
2021-01-05 07:42:11 +1030

    feat: add modules subcommands

--- src/modules/bot/commands/modules.ts
+++ src/modules/bot/commands/modules.ts
@@ -1,31 +1,130 @@
 import { Interaction, Message, MessageEmbed } from 'discord.js';
-import { Command } from '@/command';
+import { ApplicationCommandOptionType, Command } from '@/command';
 import { config } from '@/config';
-import { isTextChannelMessage } from '@/guards';
 import { moduleManager } from '@/module-manager';
+import { database } from '@/database';
+import { AppError } from '@/errors';
+import { sql } from '@databases/pg';
 
 export class Modules extends Command {
     public name = 'Modules';
     public command = 'modules';
     public timeout = 5000;
-    public description = 'Control AutoMod modules.';
+    public description = 'List all of the AutoMod modules.';
     public hidden = false;
     public owner = false;
     public examples = [];
-    public permissions = [];
+    public permissions = ['ADMINISTRATOR' as const];
+    public options = [{
+        name: 'list',
+        description: 'List all of the automod modules',
+        type: ApplicationCommandOptionType.SUB_COMMAND,
+    }, {
+        name: 'info',
+        description: 'Info on a specific module',
+        type: ApplicationCommandOptionType.SUB_COMMAND,
+        options: [{
+            name: 'module',
+            description: 'Name of the module',
+            type: ApplicationCommandOptionType.STRING,
+            required: true,
+        }]
+    }, {
+        name: 'enable',
+        description: 'Enable a module',
+        type: ApplicationCommandOptionType.SUB_COMMAND,
+        options: [{
+            name: 'module',
+            description: 'Name of the module',
+            type: ApplicationCommandOptionType.STRING,
+            required: true,
+        }]
+    }, {
+        name: 'disable',
+        description: 'Disable a module',
+        type: ApplicationCommandOptionType.SUB_COMMAND,
+        options: [{
+            name: 'module',
+            description: 'Name of the module',
+            type: ApplicationCommandOptionType.STRING,
+            required: true,
+        }]
+    }];
 
-    async messageHandler(_prefix: string, message: Message, _args: string[]) {
-        if (isTextChannelMessage(message)) {
-            return this.handler(message.guild.id);
-        }
+    async messageHandler(_prefix: string, message: Message, args: string[]) {
+        return this.handler({
+            command: args[0],
+            module: args[1]
+        }, message.guild!.id)
     }
 
     async interactionHandler(_prefix: string, interaction: Interaction) {
-        return this.handler(interaction.guild.id);
+        return this.handler({
+            command: interaction.options ? interaction.options[0].name : 'info',
+            module: interaction.options ? interaction.options[1].name : undefined,
+        }, interaction.guild.id);
+    }
+
+    async handler({ command, module: _module }: { command?: string, module?: string }, serverId: string) {
+        const handlers = {
+            enable: () => this.enableModule(serverId, _module),
+            disable: () => this.disableModule(serverId, _module),
+            list: () => this.listHandler(serverId),
+            info: () => this.infoHandler(serverId, _module),
+        };
+
+        // Invalid sub command
+        if (command !== undefined && !Object.keys(handlers).includes(command as keyof typeof handlers)) {
+            throw new AppError('Invalid sub command!');
+        }
+
+        return handlers[command as keyof typeof handlers || 'list']();
+    }
+
+    // !modules enable
+    async enableModule(serverId: string, _module?: string) {
+        if (!_module) {
+            throw new AppError('Please provide a module to enable.');
+        }
+
+        const moduleisValid = await moduleManager.getInstalledModules().then(modules => {
+            return modules.find(__module => __module.name === _module) ? true : false;
+        });
+
+        // Bail if it's not an installed module
+        if (!moduleisValid) {
+            throw new AppError('Invalid module name!');
+        }
+
+        // Set enabled in DB
+        await database.query(sql`UPDATE modules SET enabled=${true} WHERE serverId=${serverId} AND name=${_module}`);
+
+        return `Enabled ${_module} module!`;
+    }
+    
+    // !modules disable
+    async disableModule(serverId: string, _module?: string) {
+        if (!_module) {
+            throw new AppError('Please provide a module to disable.');
+        }
+
+        const moduleisValid = await moduleManager.getInstalledModules().then(modules => {
+            return modules.find(__module => __module.name === _module) ? true : false;
+        });
+
+        // Bail if it's not an installed module
+        if (!moduleisValid) {
+            throw new AppError('Invalid module name!');
+        }
+
+        // Set disable in DB
+        await database.query(sql`UPDATE modules SET enabled=${false} WHERE serverId=${serverId} AND name=${_module}`);
+
+        return `Disabled ${_module} module!`;
     }
 
     // !modules
-    async handler(serverId: string) {
+    async listHandler(serverId: string) {
         // Get all modules from db
         const modules = await moduleManager.getEnabledModules(serverId);
 
@@ -42,6 +141,32 @@ export class Modules extends Command {
             .setURL(config.PUBLIC_URL)
             .setAuthor('Modules')
             .addFields(fields);
+
+        return embed;
+    }
+
+    // !modules AutoRole
+    async infoHandler(serverId: string, moduleName?: string) {
+        if (!moduleName) {
+            throw new AppError('Please provide a module name.');
+        }
+
+        // Get all modules from db
+        const modules = await moduleManager.getEnabledModules(serverId);
+        const _module = modules.find(_module => _module.name.toLowerCase() === moduleName.toLowerCase());
+
+        if (!_module) {
+            throw new AppError('Invalid module name!');
+        }
+
+        const embed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setURL(config.PUBLIC_URL)
+            .setAuthor(_module?.name)
+            .addFields({
+                name: 'Enabled',
+                value: true
+            });
 
         return embed;
     }
