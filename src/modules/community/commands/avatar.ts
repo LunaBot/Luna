59191a4a230c08badf7a884d591622057f2ad348
Alexis Tyler <xo@wvvw.me>
2021-01-06 17:59:26 +1030

    feat: allow avatar to use ID

--- src/modules/community/commands/avatar.ts
+++ src/modules/community/commands/avatar.ts
@@ -1,5 +1,7 @@
-import { Message } from 'discord.js';
+import { Message, User } from 'discord.js';
 import { ApplicationCommandOptionType, Command } from '@/command';
+import { client } from '@/client';
+import { AppError } from '@/errors';
 
 export class Avatar extends Command {
     public name = 'Avatar';
@@ -9,7 +11,10 @@ export class Avatar extends Command {
     public hidden = false;
     public owner = false;
     public examples = [
-        '!avatar'
+        '!avatar',
+        '!avatar @OmgImAlexis#1546',
+        '!avatar <@107834314439294976>',
+        '!avatar 107834314439294976',
     ];
     public options = [{
         name: 'member',
@@ -17,19 +22,24 @@ export class Avatar extends Command {
         type: ApplicationCommandOptionType.USER,
     }];
 
-    async messageHandler(prefix: string, message: Message, args: string[]) {
-        return this.handler(prefix, message, args);
-    }
+    async messageHandler(_prefix: string, message: Message, args: string[]) {
+        // Check cache first only if it's missing do a fetch
+        const user = message.mentions.users.first() ?? args[0] ? (client.users.cache.get(args[0]) ?? await client.users.fetch(args[0])) : undefined;
 
-    // !avatar
-    // !avatar @OmgImAlexis#1546
-    // !avatar <@107834314439294976>
-    async handler(_prefix: string, message: Message, _args: string[]) {
-        const mention = message.mentions.users.first();
-        if (mention) {
-            return mention.displayAvatarURL({ format: 'png' });
+        // Couldn't resolve user
+        if (!user && args.length >= 1) {
+            throw new AppError(`Couldn't resolve user ID ${args.join(' ')}`);
         }
 
-        return message.author.displayAvatarURL({ format: 'png' });
+        // No user but they didn't try and look for one
+        // So just return their own
+        if (!user) {
+            return this.handler(message.author);
+        }
+        return this.handler(user);
+    }
+
+    async handler(user: User) {
+        return user.displayAvatarURL({ dynamic: true });
     } 
 };
