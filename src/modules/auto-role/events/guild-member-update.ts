0dfd8f5bacfa3b3f4046b43785a95d9b2be2f82b
Alexis Tyler <xo@wvvw.me>
2021-01-08 00:41:42 +1030

    refactor: cleanup AutoRole

--- src/modules/auto-role/events/guild-member-update.ts
+++ src/modules/auto-role/events/guild-member-update.ts
@@ -1,31 +1,11 @@
 import { sql } from '@databases/pg';
-import { GuildMember as DiscordGuildMember, Role } from 'discord.js';
+import { GuildMember } from 'discord.js';
 import humanizeDuration from 'humanize-duration';
 import { database } from '@/database';
-import { moduleLogger } from '@/log';
 import { sleep } from '@/utils';
-
-type GuildMember = DiscordGuildMember & {
-    pending: boolean;
-};
-
-interface AutoRole {
-    id: String;
-    serverId: String;
-    roles: String[];
-    timer: number;
-}
-
-interface RoleAndAction {
-    role: Role,
-    action: 'add' | 'remove'
-};
-
-const isRoleAndAction = (object: any): object is RoleAndAction => object.role instanceof Role;
-const log = moduleLogger.createChild({
-    prefix: 'AutoRole',
-    prefixSeperator: ']['
-});
+import { AutoRole, RoleAndAction } from '../types';
+import { isRoleAndAction } from '../guards';
+import { log } from '@/log';
 
 export const guildMemberUpdate = async (oldMember: GuildMember, newMember: GuildMember) => {
     const isMembershipScreeningEnabled = oldMember.guild.features.includes('MEMBER_VERIFICATION_GATE_ENABLED');
@@ -37,49 +17,55 @@ export const guildMemberUpdate = async (oldMember: GuildMember, newMember: Guild
         return;
     }
 
-    // Member passed membership screening or the server doesn't have it enabled
-    if (!isMembershipScreeningEnabled || (isMembershipScreeningEnabled && oldMember.pending && !newMember.pending)) {
-        log.silly('%s has now passed the membership screening.', newMember.user.username);
-
-        // Find role in discord.js cache
-        const processedAutoRoles = await Promise.all(autoRoles.map(async autoRole => {
-            const roles = await Promise.all(autoRole.roles.map(async roleIdOrName => {
-                // Remove the +/- from the start of the role
-                const action = roleIdOrName.substring(0, 1) === '+' ? 'add' : 'remove';
-                const _roleIdOrName = roleIdOrName.substr(1);
-                const role = newMember.guild.roles.cache.find(role => (role.id === _roleIdOrName) || (role.name.toLowerCase() === _roleIdOrName));
-                return {
-                    role,
-                    action
-                };
-            })).then(roles => roles.filter<RoleAndAction>(isRoleAndAction));
-
-            return {
-                timer: autoRole.timer,
-                roles
-            };
-        }));
-
-        log.silly(`Found %s role%s to apply.`, processedAutoRoles.length, processedAutoRoles.length === 1 ? '' : 's');
-
-        // Run timers and role changing
-        await Promise.all(processedAutoRoles.map(async autoRole => {
-            // If timer wait for it to finish before adding changing roles
-            if (autoRole.timer) {
-                log.silly(`Waiting ${humanizeDuration(autoRole.timer)} before applying roles to %s.`, newMember.user.username);
-                await sleep(autoRole.timer);
-            }
-
-            // Add/remove roles
-            await Promise.all(autoRole.roles.map(async ({ role, action }) => {
-                if (action === 'add') {
-                    log.silly(`Added role "%s" to %s.`, role.name, newMember.user.username);
-                    await newMember.roles.add(role);
-                } else {
-                    log.silly(`Removed role "%s" from %s.`, role.name, newMember.user.username);
-                    await newMember.roles.remove(role);
-                }
-            }));
-        }));
+    // If screening is enabled on the server
+    if (isMembershipScreeningEnabled && newMember.pending) {
+        // Bail since they haven't passed the screening and it's currenty enabled
+        return;
     }
+
+    // If screening is enabled and they've passed
+    if (isMembershipScreeningEnabled && !newMember.pending) {
+        log.silly('%s has now passed the membership screening.', newMember.user.username);
+    }
+
+    // Find role in discord.js cache
+    const processedAutoRoles = await Promise.all(autoRoles.map(async autoRole => {
+        const roles = await Promise.all(autoRole.roles.map(async roleIdOrName => {
+            // Remove the +/- from the start of the role
+            const action = roleIdOrName.substring(0, 1) === '+' ? 'add' : 'remove';
+            const _roleIdOrName = roleIdOrName.substr(1);
+            const role = newMember.guild.roles.cache.find(role => (role.id === _roleIdOrName) || (role.name.toLowerCase() === _roleIdOrName))
+            return {
+                role,
+                action
+            };
+        })).then(roles => roles.filter<RoleAndAction>(isRoleAndAction));
+
+        return {
+            timer: autoRole.timer,
+            roles
+        };
+    }));
+
+    log.silly(`Found %s role%s to apply.`, processedAutoRoles.length, processedAutoRoles.length === 1 ? '' : 's');
+
+    // Run timers and role changing
+    await Promise.all(processedAutoRoles.map(async autoRole => {
+        // If timer wait for it to finish before adding changing roles
+        if (autoRole.timer) {
+            log.silly(`Waiting ${humanizeDuration(autoRole.timer)} before applying roles to %s.`, newMember.user.username);
+            await sleep(autoRole.timer);
+        }
+
+        // Add/remove roles
+        await Promise.all(autoRole.roles.map(async ({ role, action }) => {
+            if (action === 'add') {
+                log.silly(`Added role "%s" to %s.`, role.name, newMember.user.username);
+                await newMember.roles.add(role);
+            } else {
+                log.silly(`Removed role "%s" from %s.`, role.name, newMember.user.username);
+                await newMember.roles.remove(role);
+            }
+        }));
+    }));
 };
