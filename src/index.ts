19fe4c3c9efca57b9d6aff33c16f6a3f7a0878a8
Alexis Tyler <xo@wvvw.me>
2021-01-15 19:52:18 +1030

    fix: mount endpoints in their name and add support for SPA

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Router, Application } from 'express';
+import path from 'path';
 import { envs } from './envs';
 import { AppError } from './errors';
 import * as events from './events';
@@ -6,11 +7,78 @@ import { log } from './log';
 import { client } from './client';
 import * as legacyEndpoints from './endpoints';
 import { moduleManager } from './module-manager';
-import importToArray from 'import-to-array';
+import history from 'connect-history-api-fallback';
+import crypto from 'crypto';
+import passport from 'passport';
+import expressSession from 'express-session';
+import cookieParser from 'cookie-parser';
+import serveStatic from 'serve-static';
+import bodyParser from 'body-parser';
+import * as httpStatuses from 'http-status';
+import { loadAuthentication } from './authentication';
+import { isInGuilds } from './middleware';
+
+/**
+ * Takes an imported module and converts it to an array of it's own entries.
+ * Excludes the ES6 / TypeScript "__esModule" property.
+ */
+const purifiedImports = <Key extends string, PropType>(importObject: Record<Key, PropType>): { [k: string]: PropType; } => {
+    return Object.fromEntries((Object.entries(importObject) as [string, PropType][]).filter(([key]) => !key.startsWith('__')));
+};
+
+type Endpoint = [string, Router | Endpoint];
+const processEndpoints = (app: Application) => ([name, router]: Endpoint): void => {
+    // Array of endpoints
+    if (Array.isArray(router)) {
+        return processEndpoints(app)(router);
+    }
+
+    // Single router
+    app.use(`/${name}`, router);
+};
 
 const startWebEndpoints = async () => {
     const app = express();
     const port = envs.WEB.PORT || 0;
+    const frontendFiles = path.resolve(__dirname, '..', 'frontend/dist');
+    const historyMiddleware = history({
+        verbose: true,
+        disableDotRule: true,
+        index: '/index.html'
+    });
+
+    // Serve frontend files
+    app.use(serveStatic(frontendFiles));
+
+    // Support history api 
+    app.use((req, res, next) => {
+        if (req.path.startsWith('/auth') || req.path.startsWith('/api')) {
+          next();
+        } else {
+          historyMiddleware(req, res, next);
+        }
+    });
+
+    // 2nd static files call for redirected requests
+    app.use(serveStatic(frontendFiles));
+
+    // Handle cookies, sessions, etc.
+    app.use(cookieParser());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(expressSession({
+        secret: crypto.randomBytes(64).toString('hex'),
+        resave: true,
+        saveUninitialized: true
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    // Load auth providers into passport
+    loadAuthentication();
+
+    // Check user is in guild
+    app.use(isInGuilds);
 
     // Mount module endpoints
     const modules = await moduleManager.getInstalledModules();
@@ -19,8 +87,11 @@ const startWebEndpoints = async () => {
     });
 
     // Mount legacy endpoints
-    importToArray(legacyEndpoints).forEach(endpoint => {
-        app.use(endpoint);
+    Object.entries(purifiedImports(legacyEndpoints)).forEach(processEndpoints(app));
+
+    // Show the endpoints we have registered
+    app.use('/api/endpoints', (_request, response) => {
+        return response.send(require('express-list-endpoints')(app));
     });
 
     // Mount error handler
@@ -57,7 +128,7 @@ const main = async () => {
     }
 
     // No discord owner info
-    if (!envs.OWNER.ID|| !envs.OWNER.SERVER) {
+    if (!envs.OWNER.ID || !envs.OWNER.SERVER) {
         throw new AppError('OWNER_ID and OWNER_SERVER envs both need to be set!');
     }
 
