187a9f61283e640ca4a0224c5a6d84396bfb1844
Alexis Tyler <xo@wvvw.me>
2021-01-16 17:05:57 +1030

    fix: only show servers user is a mod/admin or owner of

--- frontend/src/pages/dashboard.vue
+++ frontend/src/pages/dashboard.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <template v-if="me">
-      <span v-if="me.guilds.length === 0">Please join a guild to manage it.</span>
+      <span v-if="guilds.length === 0">Please join a guild to manage it.</span>
       <ul v-else>
-        <li v-for="guild in me.guilds" :key="guild.id">
+        <li v-for="guild in guilds" :key="guild.id">
           <router-link
             :to="{
               name: 'Modules',
@@ -20,6 +20,7 @@
 </template>
 
 <script lang="ts">
+import type { GuildInfo } from 'passport-discord';
 import { defineComponent } from 'vue';
 import store from '../store';
 
@@ -27,6 +28,17 @@ export default defineComponent({
   name: 'Dashboard',
   components: {},
   computed: {
+    guilds() {
+      // Is the owner
+      const isOwner = (guild: GuildInfo) => guild.owner;
+      // Has "ADMINISTRATOR" permission
+      const isAdmin = (guild: GuildInfo) => (guild.permissions & 0x8) === 0x8;
+      // Has "MANAGE_SERVER" permission
+      const isModerator = (guild: GuildInfo) => (guild.permissions & 0x20) === 0x20;
+
+      // Only show guilds that you own, administrate or moderate
+      return store.getters.user.guilds?.filter(guild => isOwner(guild) || isAdmin(guild) || isModerator(guild));
+    },
     me() {
       return store.getters.user;
     }
