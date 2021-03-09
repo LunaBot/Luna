189343557566548a392e5ef08beb5c07c7477aea
Alexis Tyler <xo@wvvw.me>
2021-03-10 09:29:17 +1030

    make sure to build before starting

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,32 +1,32 @@
 module.exports = {
-  apps: [
-    {
-      name: 'automod',
-      script: 'npm start',
-      time: true,
-      instances: 1,
-      autorestart: true,
-      max_restarts: 50,
-      watch: false,
-      max_memory_restart: '400M',
-      env: {
-        PORT: 3000
-      },
-    },
-  ],
-  deploy: {
-    production: {
-      user: 'xo',
-      host: '165.227.220.113',
-      key: '~/.ssh/deploy.key',
-      ref: 'origin/feat/v2',
-      repo: 'https://github.com/automodbot/automod',
-      path: '/home/xo/code/automodbot/automod',
-      'pre-deploy': 'git reset --hard',
-      'post-deploy': 'npm ci && pm2 startOrGracefulReload ecosystem.config.js --env production',
-      env: {
-        NODE_ENV: 'production'
-      },
-    },
-  },
-}
+	apps: [
+		{
+			name: 'automod',
+			script: 'npm start',
+			time: true,
+			instances: 1,
+			autorestart: true,
+			max_restarts: 50,
+			watch: false,
+			max_memory_restart: '400M',
+			env: {
+				PORT: 3000
+			}
+		}
+	],
+	deploy: {
+		production: {
+			user: 'xo',
+			host: '165.227.220.113',
+			key: '~/.ssh/deploy.key',
+			ref: 'origin/feat/v2',
+			repo: 'https://github.com/automodbot/automod',
+			path: '/home/xo/code/automodbot/automod',
+			'pre-deploy': 'git reset --hard',
+			'post-deploy': 'npm ci && npm run build && pm2 startOrGracefulReload ecosystem.config.js --env production',
+			env: {
+				NODE_ENV: 'production'
+			}
+		}
+	}
+};
