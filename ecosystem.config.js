d4d244726f506957236f0f2024265cc57a1d29e7
Alexis Tyler <xo@wvvw.me>
2021-03-09 18:30:00 +1030

    fix: use .js not .json

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
       repo: 'https://github.com/automodbot/automod',
       path: '/home/xo/code/automodbot/automod',
       'pre-deploy': 'git reset --hard',
-      'post-deploy': 'npm ci && pm2 startOrGracefulReload ecosystem.json --env production',
+      'post-deploy': 'npm ci && pm2 startOrGracefulReload ecosystem.config.js --env production',
       env: {
         NODE_ENV: 'production'
       },
