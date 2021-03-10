f0ec12584845410ea3afdac7c830bc3b6b17d4ad
Alexis Tyler <xo@wvvw.me>
2021-03-10 11:25:24 +1030

    chore: switch to logger package

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,11 +1,12 @@
 import type { GuildFeatures, Collection} from 'discord.js';
 import type Enmap from 'enmap';
+import type { Logger } from 'logger';
 
 declare module "discord.js" {
     export interface Client {
         commands: Collection<unknown, unknown>;
         settings: Enmap<string | number, any>;
-        logger: Console;
+        logger: Logger;
     }
     export interface GuildMember {
 		pending: boolean;
