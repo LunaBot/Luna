f9dafb4fbf94de69b49a0fd9dcb73f0c43027570
Alexis Tyler <xo@wvvw.me>
2021-03-11 00:05:32 +1030

    feat: update activity on read

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,11 @@
 import { client } from '../client';
 
-export const ready = () => {
+export const ready = async () => {
+	const guildsCount = client.guilds.cache.size;
+    await client.user?.setActivity({
+        name: `${guildsCount} server${guildsCount === 1 ? '' : 's'}`,
+        type: 'WATCHING'
+    });
+
 	client.logger.info('I am ready!');
 };
