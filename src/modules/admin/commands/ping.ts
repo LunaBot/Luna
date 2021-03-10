b273195352e2f0131877c5569a1695eb2044e164
Alexis Tyler <xo@wvvw.me>
2021-03-10 23:56:18 +1030

    chore: cleanup ping

--- src/modules/admin/commands/ping.ts
+++ src/modules/admin/commands/ping.ts
@@ -10,20 +10,16 @@ class Ping implements Command {
         // Bail unless we're in a guild and a member ran this
         if (!message.guild || !message.member) return;
 
-        const timeSinceMessageCreation = (Date.now() - message.createdTimestamp) - client.ws.ping;
-        const timeToProcessMessage = Date.now() - message.startedProcessingTimestamp.getTime();
-        const pingMessage = `Pong!\nIt has been \`${timeSinceMessageCreation}ms\` since you sent that command.\nIt took me \`${timeToProcessMessage}ms\` to process this command.`;
-
         // Send ping
-        const msg = await message.channel.send(new MessageEmbed({
+        const ping = await message.channel.send(new MessageEmbed({
             title: 'Pinging...',
             color: colours.ORANGE
         }));
 
         // Send pong
-        await msg.edit(new MessageEmbed({
+        await ping.edit(new MessageEmbed({
             title: 'Pong!',
-            description: "Bot Latency: ``" + Math.floor(msg.createdTimestamp - message.createdTimestamp) + " ms``\nAPI Latency: ``" + Math.round(client.ws.ping) + " ms``",
+            description: "Bot Latency: ``" + Math.floor(ping.createdTimestamp - message.createdTimestamp) + " ms``\nAPI Latency: ``" + Math.round(client.ws.ping) + " ms``",
             color: colours.GREEN
         }));
 	}
