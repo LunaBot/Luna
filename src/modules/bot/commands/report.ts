793ac936f093174c30c641aa6cfe05e70796811c
Alexis Tyler <xo@wvvw.me>
2021-03-11 17:24:37 +1030

    feat: use embed for reports

--- src/modules/bot/commands/report.ts
+++ src/modules/bot/commands/report.ts
@@ -1,6 +1,8 @@
 import { Command } from '../../../command';
 import type { Message, Client } from 'discord.js';
 import { Collection } from 'discord.js';
+import { MessageEmbed } from 'discord.js';
+import { CommandError } from '../../../errors';
 
 class Report implements Command {
     public name = 'report';
@@ -14,11 +16,30 @@ class Report implements Command {
         // Bail unless we're in a guild and a member ran this
         if (!message.guild || !message.member) return;
 
-        // Relay report to my self
-        // @todo: turn this into an embed
-        // @todo: move the id to a dynamic location instead of hardcoding it
+        // Get guild config
+        const guildConfig = client.settings.get(message.guild.id)!;
+
+        // Report can only be 1000 characters
+        const charactersLength = (message.content.length - guildConfig.prefix.length - 7);
+		if (charactersLength >= 1000) {
+			throw new CommandError(`Your report is ${charactersLength - 1000} characters too long, please try again.`);
+		}
+
+        // Relay report to the bot's owner
+        // @todo: move the bot owner's id to a dynamic location instead of hardcoding it
         const dmChannel = message.guild.members.cache.get('107834314439294976');
-        await dmChannel?.send(message.content);
+        await dmChannel?.send(new MessageEmbed({
+            author: {
+                name: message.author.username,
+                iconURL: message.author.displayAvatarURL(),
+            },
+            fields: [{
+                name: 'Report',
+                value: message.content.substring(guildConfig.prefix.length + 7)
+            }]
+        }));
+
+        await message.reply('Report sent!');
 	}
 };
 
