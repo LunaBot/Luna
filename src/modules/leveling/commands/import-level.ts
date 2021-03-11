63b4a4a1b4fd8470543ae07bddecef36ca369af7
Alexis Tyler <xo@wvvw.me>
2021-03-11 15:30:07 +1030

    feat: add leveling/import-level command

---
+++ src/modules/leveling/commands/import-level.ts
@@ -0,0 +1,43 @@
+import { Command } from '../../../command';
+import type { Message, Client } from 'discord.js';
+import { Pool } from 'pg';
+import { CommandError } from '../../../errors';
+import { experienceToLevel } from '../../../utils';
+
+const db = new Pool({
+    ssl: {
+        rejectUnauthorized: false,
+    }
+});
+
+class ImportLevel implements Command {
+    public name = 'import-level';
+
+    async run(client: Client, message: Message, args: string[]) {
+        // Bail unless we're in a guild and a member ran this
+        if (!message.guild || !message.member) return;
+
+        try {
+
+            // Import XP from old DB
+            const response = await db.query('SELECT * FROM users WHERE serverId=$1::text AND id=$2::text', [message.guild.id, message.author.id]);
+
+            // No user found
+            if (response.rows.length === 0) throw new CommandError('No user found in old DB, import failed!');
+
+            // Get old XP
+            const experience = parseInt(response.rows[0].experience);
+
+            // Update new DB
+            client.points.set(`${message.guild.id}-${message.author.id}`, experience, 'experience');
+            client.points.set(`${message.guild.id}-${message.author.id}`, experienceToLevel(experience), 'level');
+
+            // Reply to user
+            await message.channel.send('Imported XP!');
+        } catch (error: unknown) {
+            client.logger.debug('Failed importing XP for %s with "%s', message.author.id, (error as Error).message);
+        }
+    }
+}
+
+export const importLevel = new ImportLevel();
