7c993ea200f134a4be1b4eabe9adf35867455535
Alexis Tyler <xo@wvvw.me>
2021-03-11 15:29:52 +1030

    fix: leveling not ensuring correct field

--- src/modules/leveling/events/message.ts
+++ src/modules/leveling/events/message.ts
@@ -16,7 +16,6 @@ const getExperience = (client: Client, message: Message) => {
     const baseXp = 20;
     const sentimentPercentage = sentiment / 100;
     const experience = capValue(1 + (baseXp * sentimentPercentage), -20, 20);
-    client.logger.silly('%s gained %s exp for "%s"', message.author.tag, experience, message.content);
     return experience;
 };
 
@@ -40,10 +39,17 @@ export const message = async (client: Client, message: Message) => {
         return;
     }
 
+    // Create named logger with id and name
+    const logger = client.logger.createChild({
+        prefix: message.guild.id
+    }).createChild({
+        prefix: message.guild.name
+    });
+
     // Bail if it's a command
     const guildConfig = client.settings.get(message.guild.id)!;
     if (message.content.indexOf(guildConfig.prefix) === 0) {
-        client.logger.silly('%s gained 0 exp for "%s" as it looks like a command.', message.author.tag, message.content);
+        logger.silly('%s gained 0 exp for "%s" as it looks like a command.', message.author.tag, message.content);
         return;
     }
 
@@ -54,17 +60,19 @@ export const message = async (client: Client, message: Message) => {
     client.points.ensure(pointsKey, {
         user: message.author.id,
         guild: message.guild.id,
-        points: 0,
+        experience: 0,
         level: 1
     });
 
+    // Give experience to user 
     const experience = getExperience(client, message);
     client.points.math(pointsKey, '+', experience, 'experience');
+    logger.silly('%s gained %s exp for "%s"', message.author.tag, experience, message.content);
 
     // Calculate the user's current level
     const curLevel = experienceToLevel(client.points.get(pointsKey, 'experience'));
 
-    // Act upon level up by sending a message and updating the user's level in enmap.
+    // Act upon level up by sending a message and updating the user's level in the db.
     if (client.points.get(pointsKey, 'level') < curLevel) {
         // Update points
         client.points.set(pointsKey, curLevel, 'level');
