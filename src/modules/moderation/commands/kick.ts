b3148263ef0eaa86edd2ce912a4c7753333fb1e0
Alexis Tyler <xo@wvvw.me>
2021-03-11 19:42:57 +1030

    feat: add kick command

--- src/modules/moderation/commands/kick.ts
+++ src/modules/moderation/commands/kick.ts
@@ -3,6 +3,9 @@ import type { Message, Client } from 'discord.js';
 import { isAdmin, isOwner } from '../../../utils';
 import { CommandError } from '../../../errors';
 import { Collection } from 'discord.js';
+import { MessageEmbed } from 'discord.js';
+import { createChannel } from '../../audit-log/utils/create-channel';
+import { TextChannel } from 'discord.js';
 
 class Kick implements Command {
     public name = 'kick';
@@ -22,6 +25,66 @@ class Kick implements Command {
     	// Command is owner/admin only
         if (!isOwner(message.guild, message.member) && !isAdmin(message.guild, message.member)) {
             throw new CommandError('You\'re not an admin or the owner, sorry!');
+        }
+
+        // Get guild config
+        const guildConfig = client.settings.get(message.guild.id)!
+
+        // Create named logger with id and name
+        const logger = client.logger.createChild({
+            prefix: message.guild.id
+        }).createChild({
+            prefix: message.guild.name
+        });
+
+        // Get the user we need to kick
+        const memberToKick = message.mentions.members?.first();
+
+        // Bail if we don't have someone to kick
+        if (!memberToKick) throw new CommandError('You need to mention a user to kick them!');
+
+        // Find the channel
+        const auditLog = message.guild.channels.cache.find(channel => channel.name === (guildConfig.auditLog.channel ?? 'audit-log')) as TextChannel;
+
+        // If we can't find the channel then create one called "audit-log"
+        if (!auditLog) {
+            await createChannel(client, message);
+        }
+
+        // If there's something other than just a mention let's use it as a reason
+        const reason = args.length >= 2 ? args.slice(1).join(' ') : '';
+
+        // Build fields
+        const fields: { name: string; value: string; }[] = [];
+        if (reason) {
+            fields.push({
+                name: 'Reason',
+                value: reason
+            });
+        }
+
+        try {
+            // DM user
+            await memberToKick.send(new MessageEmbed({
+                description: `:woman_police_officer: **You were kicked from ${message.guild.name}**`,
+                fields,
+                timestamp: new Date()
+            }))
+
+            // Try and kick member with reason
+            await memberToKick.kick(reason);
+            
+            // Log for debugging
+            logger.silly(`${message.author.tag} kicked ${memberToKick.user.tag || memberToKick.user.username}`);
+
+            // Send message to audit log channel
+            await auditLog.send(new MessageEmbed({
+                description: `:woman_police_officer: <@${memberToKick.user?.id}> **was kicked by** <@${message.member.user.id}>`,
+                fields,
+                timestamp: new Date()
+            }));
+        } catch (error: unknown) {
+            await message.channel.send(`Failed kicking **${memberToKick.user.tag}**: ${(error as Error).message}`);
         }
     }
 }
