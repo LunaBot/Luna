5ed43d6c799dd249b458cc1eb945a6d500ac693a
Alexis Tyler <xo@wvvw.me>
2021-03-12 13:06:50 +1030

    chore: add more info to startup log

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,14 +3,19 @@ import { statcord } from '../statcord';
 
 export const ready = async () => {
 	const guildsCount = client.guilds.cache.size;
-    await client.user?.setActivity({
+
+    // Idk what happened?
+    if (!client.user) return;
+
+    // Set activity
+    await client.user.setActivity({
         name: `${guildsCount} server${guildsCount === 1 ? '' : 's'}`,
         type: 'WATCHING'
     });
 
     // Log for debugging
-	client.logger.info('I am ready!');
+	client.logger.info(`${client.user.tag}, Ready to serve ${client.guilds.cache.size} guilds and ${client.users.cache.size} users`);
 
-    // Start auto posting
+    // Start auto posting to statcord
     statcord.autopost();
 };
