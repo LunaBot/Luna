a49fd3b9aa0c520db5e433185c89e90e6a0f2046
Alexis Tyler <xo@wvvw.me>
2021-03-13 21:46:19 +1030

    feat: command help

--- src/modules/bot/commands/help.ts
+++ src/modules/bot/commands/help.ts
@@ -2,10 +2,15 @@ import { Command } from '../../../command';
 import type { Message, Client } from 'discord.js';
 import { MessageEmbed, Collection } from 'discord.js';
 import camelcase from 'camelcase';
+import { report } from './report';
 
 class Help implements Command {
     public name = 'help';
     public paramaters = new Collection<string, { type: 'string' | 'boolean' | 'number' | 'mention'; }>();
+
+    getHelpText() {
+        return 'Get help text.';
+    }
 
     async run(client: Client, message: Message, args: string[]) {
         // Bail unless we're in a guild and a member ran this
@@ -14,28 +19,70 @@ class Help implements Command {
         // Get guild config
         const guildConfig = client.settings.get(message.guild.id)!;
 
-        // Send help
+        // Get command
+        const commandName = args[0];
+
+        // Send general help
+        if (!commandName) {
+            await message.channel.send(new MessageEmbed({
+                title: 'Help',
+                fields: client.modules.filter(commandModule => {
+                    const moduleKey = camelcase(commandModule.name);
+                    const isEnabled = Object.keys(guildConfig).includes(moduleKey) && guildConfig[moduleKey].enabled;
+                    const hasCommands = Object.keys(commandModule.commands).length >= 1;
+                    return isEnabled && hasCommands;
+                }).map(commandModule => {
+                    return {
+                        name: `**__${commandModule.name}__**`,
+                        value: `${Object.values(commandModule.commands).map(command => {
+                            const paramaters = (!command.paramaters || command.paramaters.size === 0) ? '' : (' ' + [...command.paramaters.entries()].map(([key, parameter]) => {
+                                const brackets = parameter.optional ? '[]' : '<>';
+                                return `${brackets[0]}${parameter.type === 'mention' ? '@' : ''}${key}${brackets[1]}`;
+                            }).join(' '));
+                            return '`' + guildConfig.prefix + command.name + paramaters + '`';
+                        }).join('\n')}\n\n` + `Use \`${guildConfig.prefix}${report.name} <message>\` to report any bugs.`
+                    };
+                })
+            }));
+        }
+
+        // Get command
+        const command = client.commands.get(commandName);
+
+        // Bail if the command name is invalid
+        if (!command) return;
+
+        // Build command params
+        const paramaters = (!command.paramaters || command.paramaters.size === 0) ? '' : (' ' + [...command.paramaters.entries()].map(([key, parameter]) => {
+            const brackets = parameter.optional ? '[]' : '<>';
+            return `${brackets[0]}${parameter.type === 'mention' ? '@' : ''}${key}${brackets[1]}`;
+        }).join(' '));
+
+        // Get command's module
+        const commandModule = client.modules.find(commandModule => {
+            const found = Object.values(commandModule.commands).find(command => command.name === commandName);
+            return Boolean(found);
+        });
+
+        // Bail if command module can't be found
+        if (!commandModule) return;
+
+        // Guild help text
+        const getHelpText = () => command.getHelpText ? command.getHelpText(client, message) : false;
+        const description = getHelpText() || `No help text available, please report this with the \`${guildConfig.prefix}${report.name}\` command.`;
+
+        // Send command help
         await message.channel.send(new MessageEmbed({
-            title: 'Help',
-            description: `Use \`${guildConfig.prefix}report <message>\` to report any bugs.`,
-            fields: client.modules.filter(commandModule => {
-                const moduleKey = camelcase(commandModule.name);
-                const isEnabled = Object.keys(guildConfig).includes(moduleKey) && guildConfig[moduleKey].enabled;
-                const hasCommands = Object.keys(commandModule.commands).length >= 1;
-                return isEnabled && hasCommands;
-            }).map(commandModule => {
-                return {
-                    name: `**__${commandModule.name}__**`,
-                    value: `${Object.values(commandModule.commands).map(command => {
-                        const paramaters = (!command.paramaters || command.paramaters.size === 0) ? '' : (' ' + [...command.paramaters.entries()].map(([key, parameter]) => {
-                            const brackets = parameter.optional ? '[]' : '<>';
-                            return `${brackets[0]}${parameter.type === 'mention' ? '@' : ''}${key}${brackets[1]}`;
-                        }).join(' '));
-                        return '`' + guildConfig.prefix + command.name + paramaters + '`';
-                    }).join('\n')}`
-                };
-            })
+            title: `Help`,
+            fields: [{
+                name: `**__command__**`,
+                value: '`' + guildConfig.prefix + command.name + paramaters + '`'
+            }, {
+                name: `**__description__**`,
+                value: description + '\n\n' + `Use \`${guildConfig.prefix}${report.name} <message>\` to report any bugs.`
+            }]
         }))
+
     }
 };
 
